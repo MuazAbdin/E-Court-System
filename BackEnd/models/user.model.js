f80c71530796d804668fda901ddb5b528739cc14
jaafari-R <jaafariramadan@gmail.com>
2024-03-05 21:25:02 +0200

    Added base User schema

---
+++ BackEnd/models/user.model.js
@@ -0,0 +1,21 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema({
+    idNumber: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    userType: { type: Schema.Types.ObjectId, ref: 'UserType', required: true } ,
+    email: { type: String, required: true, unique: true},
+    phoneNumber: { type: String, required: true },
+    city: { type: String, required: true } ,
+    street: { type: String, required: true },
+    licenseNumber: { type: String, default: null },
+    documents: { 
+        type: [{ type: Schema.Types.ObjectId, ref: 'Document' }],
+        default: []
+    }
+})
+
+const User = model("User", userSchema, "User");
+
+export default User;
