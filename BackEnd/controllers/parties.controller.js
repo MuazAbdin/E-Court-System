97ce0ccf3090ced25e66ac4496288ee92145e1a4
jaafari-R <jaafariramadan@gmail.com>
2024-03-06 00:57:34 +0200

    Added Parties routes and initialized their controller

---
+++ BackEnd/controllers/parties.controller.js
@@ -0,0 +1,24 @@
+class PartiesController {
+    createParty(req, res) {
+
+    }
+
+    getPartyById(req, res) {
+
+    }
+
+    getPartyByCaseId(req, res) {
+
+    }
+
+    updateParty(req, res) {
+
+    }
+
+    deleteParty(req, res) {
+        
+    }
+}
+
+const partiesController = new PartiesController();
+export default partiesController;
