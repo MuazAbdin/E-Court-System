01139881a36ffa1707eecf08fa5f78fcbdfe65ec
jaafari-R <jaafariramadan@gmail.com>
2024-03-06 00:56:53 +0200

    Added Users routes and initialized their controller

---
+++ BackEnd/controllers/users.controller.js
@@ -0,0 +1,24 @@
+class UserController {
+    getJudges(req, res) {
+
+    }
+
+    getLawyers(req, res) {
+
+    }
+
+    getUsers(req, res) {
+
+    }
+
+    updateAllUserData(req, res) {
+
+    }
+
+    updateUser(req, res) {
+        
+    }
+}
+
+const userController = new UserController();
+export default userController;
