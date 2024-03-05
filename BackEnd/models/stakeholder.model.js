df8389b38df7beb96aecd56d5c657d772dcb59d4
jaafari-R <jaafariramadan@gmail.com>
2024-03-05 23:05:44 +0200

    Added base Stakeholder schema

--- BackEnd/models/stakeholder.model.js
+++ BackEnd/models/stakeholder.model.js
@@ -1,14 +1,14 @@
 import { model, Schema } from 'mongoose';
 
 const stakeholderSchema = new Schema({
-    party,
-    idNumber,
-    firstName,
-    lastName,
-    email,
-    phoneNumber,
-    city,
-    street
+    party: { type: Schema.Types.ObjectId, ref: 'Party', required: true },
+    idNumber: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, lowercase: true },
+    phoneNumber: { type: String, required: true },
+    city: { type: String, required: true },
+    street: { type: String, required: true }
 })
 
 const Stakeholder = model("Stakeholder", stakeholderSchema, "Stakeholder");
