d9b3ee5ed0ddc3a53e9bb3b1f7ad95d7ced8ccb1
jaafari-R <jaafariramadan@gmail.com>
2024-03-05 23:46:07 +0200

    Added Courts routes and initialized their controller

---
+++ BackEnd/controllers/courts.controller.js
@@ -0,0 +1,16 @@
+class CourtsController {
+    createCourt(req, res) {
+
+    }
+
+    getCourtById(req, res) {
+
+    }
+
+    updateCourt(req, res) {
+        
+    }
+}
+
+const courtsController = new CourtsController();
+export default courtsController;
