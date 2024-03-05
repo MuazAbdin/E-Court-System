ecc9517ebc8dfd707db02e7f65c177e24b2ac90c
jaafari-R <jaafariramadan@gmail.com>
2024-03-05 22:26:22 +0200

    Added base Case schema

---
+++ BackEnd/models/case.model.js
@@ -0,0 +1,35 @@
+import { model, Schema } from 'mongoose';
+import { setFieldCounter, getCounterAndIncrement } from '../utils/db.utils';
+
+const caseSchema = new Schema({
+    caseNumber: { type: Number, required: true, unique: true, default: getCaseNumber },
+    status: { type: Schema.Types.ObjectId, ref: "CaseStatusType", required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    court: { type: Schema.Types.ObjectId, ref: "Court", required: true },
+    judges: { 
+        type: [{ type: Schema.Types.ObjectId, ref: "User" }],
+        default: []
+    },
+    creationDate: { type: Date, default: Date.now },
+    lastUpdateDate: { type: Date, default: null },
+    events: {
+        type: [{ type: Schema.Types.ObjectId, ref: "Event" }],
+        default: []
+     },
+    parties: {
+        type: [{ type: Schema.Types.ObjectId, ref: "Party" }],
+        default: []
+     }
+})
+
+caseSchema.statics.caseNumberCounter = 1;
+
+const Case = model("Case", caseSchema, "Case");
+setFieldCounter(Case, "caseNumber", "caseNumberCounter", 1);
+
+function getCaseNumber() {
+    return getCounterAndIncrement(Case, "caseNumberCounter");
+}
+
+export default Case;
