0293b458ab2d220fc33c028b4230d9b4c60fefa7
jaafari-R <jaafariramadan@gmail.com>
2024-03-06 16:02:00 +0200

    renamed Routers to Routes

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,17 +1,17 @@
 import express from "express";
 import cors from "cors";
 
-import { router as userTypesRouter } from "./routes/userTypes.route.js";
-import { router as usersRouter } from "./routes/users.route.js";
-import { router as authRouter } from "./routes/auth.route.js";
-import { router as casesRouter } from "./routes/cases.route.js";
-import { router as caseStatusTypesRouter } from "./routes/caseStatusTypes.route.js";
-import { router as courtsRouter } from "./routes/courts.route.js";
-import { router as documentsRouter } from "./routes/documents.route.js";
-import { router as eventsRouter } from "./routes/events.route.js";
-import { router as eventTypesRouter } from "./routes/eventTypes.route.js";
-import { router as partiesRouter } from "./routes/parties.route.js";
-import { router as stakeholdersRouter } from "./routes/stakeholders.route.js";
+import { router as userTypesRoutes } from "./routes/userTypes.route.js";
+import { router as usersRoutes } from "./routes/users.route.js";
+import { router as authRoutes } from "./routes/auth.route.js";
+import { router as casesRoutes } from "./routes/cases.route.js";
+import { router as caseStatusTypesRoutes } from "./routes/caseStatusTypes.route.js";
+import { router as courtsRoutes } from "./routes/courts.route.js";
+import { router as documentsRoutes } from "./routes/documents.route.js";
+import { router as eventsRoutes } from "./routes/events.route.js";
+import { router as eventTypesRoutes } from "./routes/eventTypes.route.js";
+import { router as partiesRoutes } from "./routes/parties.route.js";
+import { router as stakeholdersRoutes } from "./routes/stakeholders.route.js";
 
 const app = express();
 
@@ -19,16 +19,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use("/auth", authRouter);
-app.use("/users", usersRouter);
-app.use("/cases", casesRouter);
-app.use("/case-status-types", caseStatusTypesRouter);
-app.use("/courts", courtsRouter);
-app.use("/documents", documentsRouter);
-app.use("/events", eventsRouter);
-app.use("/eventTypes", eventTypesRouter);
-app.use("/parties", partiesRouter);
-app.use("/stakeholders", stakeholdersRouter);
-app.use("/user-types", userTypesRouter);
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+app.use("/cases", casesRoutes);
+app.use("/case-status-types", caseStatusTypesRoutes);
+app.use("/courts", courtsRoutes);
+app.use("/documents", documentsRoutes);
+app.use("/events", eventsRoutes);
+app.use("/eventTypes", eventTypesRoutes);
+app.use("/parties", partiesRoutes);
+app.use("/stakeholders", stakeholdersRoutes);
+app.use("/user-types", userTypesRoutes);
 
 export default app;
