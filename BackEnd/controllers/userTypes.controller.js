c17a804476f3ae95d52dffd13e476746c94ae8df
jaafari-R <jaafariramadan@gmail.com>
2024-03-07 00:54:51 +0200

    Implemented getAllUserTypes controller

--- BackEnd/controllers/userTypes.controller.js
+++ BackEnd/controllers/userTypes.controller.js
@@ -1,4 +1,5 @@
 import errorHandler from "../errors/errorHandler.js";
+import { NoUserTypesFoundError } from "../errors/userType.error.js";
 import UserType from "../models/userType.model.js";
 import UserTypesValidator from "../validators/userTypes.validate.js";
 
@@ -15,8 +16,16 @@ class UserTypesController {
         }
     }
 
-    getAllUserTypes(req, res) {
-        res.status(404).send("Work In Progress!");
+    async getAllUserTypes(req, res) {
+        try {
+            const userTypes = await UserType.find();
+            if(userTypes.length === 0)
+                throw new NoUserTypesFoundError();
+            res.send(userTypes);
+        }
+        catch(error) {
+            errorHandler.handleError(res, error);
+        }
     }
 
     updateUserType(req, res) {
