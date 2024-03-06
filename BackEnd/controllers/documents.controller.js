3977310150c96d1429fb7c1e929bd14a19a196b7
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-03-06 19:01:24 +0200

    fix identation

--- BackEnd/controllers/documents.controller.js
+++ BackEnd/controllers/documents.controller.js
@@ -1,27 +1,25 @@
 class DocumentsController {
-    createDocument(req, res) {
+	createDocument(req, res) {}
 
-    }
+	getDocumentById(req, res) {
+		res.status(404).send("Work In Progress!");
+	}
 
-    getDocumentById(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
+	getDocumentByPartyId(req, res) {
+		res.status(404).send("Work In Progress!");
+	}
 
-    getDocumentByPartyId(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
+	getDocumentByCaseId(req, res) {
+		res.status(404).send("Work In Progress!");
+	}
 
-    getDocumentByCaseId(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
+	getDocumentByUserId(req, res) {
+		res.status(404).send("Work In Progress!");
+	}
 
-    getDocumentByUserId(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
-
-    updateDocumentTitle(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
+	updateDocumentTitle(req, res) {
+		res.status(404).send("Work In Progress!");
+	}
 }
 
 const documentsController = new DocumentsController();
