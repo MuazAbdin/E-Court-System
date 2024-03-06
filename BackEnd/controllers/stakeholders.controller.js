75228cab3324fdeb286d6ee6bf8efb1e376045e1
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-03-06 18:24:36 +0200

    finish get routes

--- BackEnd/controllers/stakeholders.controller.js
+++ BackEnd/controllers/stakeholders.controller.js
@@ -1,23 +1,37 @@
+import Stakeholder from "../models/stakeholder.model";
+
 class StakeholdersController {
-    createStakeholder(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
+	createStakeholder(req, res) {
+		res.status(404).send("Work In Progress!");
+	}
 
-    getStakeholderById(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
+	async getStakeholderById(req, res) {
+		const { stackHoldlerId } = req.params;
+		try {
+			const stackHoldler = await Stakeholder.findById(stackHoldlerId);
+			res.json(stackHoldler);
+		} catch {
+			res.sendStatus(500);
+		}
+	}
 
-    getStakeholderByPartyId(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
+	async getStakeholderByPartyId(req, res) {
+		const { partyId } = req.params;
+		try {
+			const stackHoldler = await Stakeholder.find({ party: partyId });
+			res.json(stackHoldler);
+		} catch {
+			res.sendStatus(500);
+		}
+	}
 
-    updateStakeholder(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
+	updateStakeholder(req, res) {
+		res.status(404).send("Work In Progress!");
+	}
 
-    deleteStakeholder(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
+	deleteStakeholder(req, res) {
+		res.status(404).send("Work In Progress!");
+	}
 }
 
 const stakeholdersController = new StakeholdersController();
