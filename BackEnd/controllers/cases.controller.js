ac3d4f110d7b509759299b31f8280e578537e661
Your username <ek29091@gmail.com>
2024-03-07 14:23:20 +0200

    add code

--- BackEnd/controllers/cases.controller.js
+++ BackEnd/controllers/cases.controller.js
@@ -6,10 +6,22 @@ class CasesController {
 	getCases(req, res) {
 		res.status(404).send("Work In Progress!");
 	}
-
-	getCaseById(req, res) {
-		res.status(404).send("Work In Progress!");
-	}
+	async getCaseById(req, res) {
+        const { id } = req.params;
+        try {
+            const case_ = Case.findById(id);
+			if(!case_) {
+				throw new CaseDoesNotExistError();
+			}
+            res.json(case_);
+        }
+        catch(error) {
+            errorHandler.handleError(res, error);
+        }
+    }
+	// getCaseById(req, res) {
+	// 	res.status(404).send("Work In Progress!");
+	// }
 
 	updateCase(req, res) {
 		res.status(404).send("Work In Progress!");
--- BackEnd/controllers/users.controller.js
+++ BackEnd/controllers/users.controller.js
@@ -1,15 +1,40 @@
 class UserController {
-    getJudges(req, res) {
-        res.status(404).send("Work In Progress!");
+    async getJudges(req, res) {
+        try {
+            const judges = await User.find({ role: 'judge' });
+            if (judges.length === 0) {
+                return res.status(404).send("Work In Progress!");
+            }
+            res.json(judges);
+        } catch (error) {
+            errorHandler.handleError(res, error);
+        }
     }
 
-    getLawyers(req, res) {
-        res.status(404).send("Work In Progress!");
+       async getLawyers(req, res) {
+        try {
+            const lawyers = await User.find({ role: 'lawyer' });
+            if (lawyers.length === 0) {
+                return res.status(404).send("Work In Progress!");
+            }
+            res.json(lawyers);
+        } catch (error) {
+            errorHandler.handleError(res, error);
+        }
+    }
+    async getUsers(req, res) {
+        try {
+            const users = await User.find({});
+            if (users.length === 0) {
+               
+                return res.status(404).send("Work In Progress!");
+            }
+            res.json(users);
+        } catch (error) {
+            errorHandler.handleError(res, error);
+        }
     }
 
-    getUsers(req, res) {
-        res.status(404).send("Work In Progress!");
-    }
 
     updateAllUserData(req, res) {
         res.status(404).send("Work In Progress!");
