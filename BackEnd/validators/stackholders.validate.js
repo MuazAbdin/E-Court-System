debe710b8711668120ab9254f67b659c813f1154
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-03-07 19:02:39 +0200

    update

--- BackEnd/validators/stackholders.validate.js
+++ BackEnd/validators/stackholders.validate.js
@@ -1,17 +1,25 @@
 import GenericValidator from "./generic.validate.js";
 
-
-
-
-
-
-
 export default class StackHolderValidator {
-	static validateCourtData(data) {
+	static validateStackHolderData(data) {
 		Object.keys(data).forEach((key) =>
 			GenericValidator.validateNotEmpty(data[key])
 		);
 		GenericValidator.validateEmail(data.email);
 		GenericValidator.validatePhoneNumber(data.phoneNumber);
+		GenericValidator.validateIdNumber(data.idNumber);
 	}
 }
+
+// async createStackHolder(req, res) {
+// 		    const { idNumber, firstName, lastName, email, phoneNumber, city, street } = req.body;
+// 		try {
+// 			StackHolderValidator.validateStackHolderData(req.body);
+
+// 			const court = await StackHolder.create({ name: courtName, city, street, phoneNumber, email });
+// 			res.json(court);
+// 		}
+// 		catch(error) {
+// 			errorHandler.handleError(res, error);
+// 		}
+// 	}
