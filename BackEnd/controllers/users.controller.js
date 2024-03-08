74c57f4a486d5afbe53399c60ee747d855675a19
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-03-08 23:42:05 +0200

    first commit

--- BackEnd/controllers/users.controller.js
+++ BackEnd/controllers/users.controller.js
@@ -1,3 +1,8 @@
+import UserValidator from "../validators/user.validate";
+import User from "../models/user.model.js"
+import { UserDoesNotExistError } from "../errors/user.error.js";
+import GenericValidator from "../validators/generic.validate.js";
+
 class UserController {
     getJudges(req, res) {
         res.status(404).send("Work In Progress!");
@@ -15,8 +20,20 @@ class UserController {
         res.status(404).send("Work In Progress!");
     }
 
-    updateUser(req, res) {
-        res.status(404).send("Work In Progress!");
+   async updateUser(req, res) {
+       const { _id, phoneNumber, city, street } = req.body
+		try {
+            GenericValidator.validateObjectId(_id);
+			UserValidator.validateUserData({ phoneNumber, city, street });
+			const updateUser = await User.findByIdAndUpdate(_id, {$set: { phoneNumber, city, street }}, { new: true });
+			if(updateUser === null) {
+				throw new UserDoesNotExistError();
+			}
+			res.json(updateUser);
+		}
+		catch(error) {
+			errorHandler.handleError(res, error);
+		}
     }
 }
 
