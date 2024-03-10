906500b25e671e6d1bf8a12d4c7e10c8c38ea144
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-03-10 19:16:56 +0200

    finished create and getById document

--- BackEnd/controllers/documents.controller.js
+++ BackEnd/controllers/documents.controller.js
@@ -2,12 +2,33 @@ import Document from "../models/document.model.js"
 import { DocumentDoesNotExistError } from "../errors/document.error.js";
 import errorHandler from "../errors/errorHandler.js";
 import DocumentsValidator from "../validators/documents.validate.js";
+import GenericValidator from "../validators/generic.validate.js";
 
 class DocumentsController {
-	createDocument(req, res) {}
+    async createDocument(req, res) {
+		const { caseId, title, uploadedBy } = req.body
+	try {
+			DocumentsValidator.validateDocumentData(req.body);
+			const document = await Document.create({ case: caseId, title, uploadedBy , fileLocation: "test", fileName: "test" });
+			res.json(document);
+		}
+		catch(error) {
+			errorHandler.handleError(res, error);
+		}
+	}
 
-	getDocumentById(req, res) {
-		res.status(404).send("Work In Progress!");
+	async getDocumentById(req, res) {
+		const { id } = req.params;
+		try {
+			GenericValidator.validateObjectId(id);
+			const document = await Document.findById(id);
+			if(document === null) {
+				throw new DocumentDoesNotExistError();
+			}
+			res.json(document);
+		} catch(error) {
+			return errorHandler.handleError(res, error)
+		}
 	}
 
 	getDocumentByPartyId(req, res) {
@@ -25,7 +46,7 @@ class DocumentsController {
 	async updateDocumentTitle(req, res) {
 		const { id, title } = req.body;
 		try {
-			DocumentsValidator.validateDocumentData({ id, title });
+			DocumentsValidator.validateUpdateDocumentData({ id, title });
 			const updatedDocument = await Document.findByIdAndUpdate(id, {$set: { title }});
 			if(updatedDocument === null) {
 				throw new DocumentDoesNotExistError();
