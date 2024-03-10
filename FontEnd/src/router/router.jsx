ebe18663ea46f006c3afe7a25b1e24d31f32ce90
RawabyRahal <rawaby.16.4@gmail.com>
2024-03-10 18:38:21 +0200

    connecting the forms with the BACKEND and refactoring them

--- FontEnd/src/router/router.jsx
+++ FontEnd/src/router/router.jsx
@@ -18,6 +18,10 @@ import StakeholderForm from "../pages/StakeholderForm";
 
 import { action as authAction } from "../pages/Authentication";
 import { action as caseAction } from "../pages/CaseForm";
+import { action as courtAction } from "../pages/CourtForm";
+import { action as documentCreation } from "../pages/DocumentForm";
+import { action as partyCreation } from "../pages/PartyForm";
+import { action as stakeholderCreation } from "../pages/StakeholderForm";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -43,6 +47,7 @@ const router = createBrowserRouter([
           {
             path: "document/:caseId",
             element: <DocumentForm />,
+            action: documentCreation
           },
           {
             path: "case",
@@ -60,14 +65,17 @@ const router = createBrowserRouter([
           {
             path: "court",
             element: <CourtForm />,
+            action: courtAction,
           },
           {
             path: "party",
             element: <PartyForm />,
+            action: partyCreation
           },
           {
             path: "stakeholder",
             element: <StakeholderForm />,
+            action: stakeholderCreation
           },
         ],
       },
