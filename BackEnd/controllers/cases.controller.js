9f7fd13137937a32a452b58f93abae7ebf796939
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-03-11 13:39:38 +0200

    first commit

--- BackEnd/controllers/cases.controller.js
+++ BackEnd/controllers/cases.controller.js
@@ -1,6 +1,8 @@
 import errorHandler from "../errors/errorHandler.js";
 import Case from "../models/case.model.js";
-import { NoCasesFoundError } from "../errors/case.error.js";
+import { CaseDoesNotExistError, InvalidCaseStatusError, NoCasesFoundError } from "../errors/case.error.js";
+import GenericValidator from "../validators/generic.validate.js";
+import CaseValidator from "../validators/case.validate.js";
 
 class CasesController {
 	createCase(req, res) {
@@ -25,8 +27,24 @@ class CasesController {
 		res.status(404).send("Work In Progress!");
 	}
 
-	updateCase(req, res) {
-		res.status(404).send("Work In Progress!");
+	async updateCase(req, res) {
+		const { caseId, title, description, status, court, judge } = req.body
+		try{
+			GenericValidator.validateObjectId(caseId);
+			CaseValidator.validateCaseData({ title, description, status, court, judge });
+			const updatedCase = await Case.findByIdAndUpdate(caseId, {$set: { title, description, status, court, judge }}, { new: true });
+			if(updatedCase === null) {
+				throw new CaseDoesNotExistError();
+			}
+			res.json(updatedCase);
+		} catch(error) {
+			if(error instanceof mongoose.Error.ValidationError) {
+				if(error.errors.status) {
+					return errorHandler.handleError(res, new InvalidCaseStatusError());
+				}
+			}
+			errorHandler.handleError(res, error);
+		}
 	}
 
 	updateCaseStatus(req, res) {
