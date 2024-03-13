b495e5d0baf4dbdc2116f37bdfd8ac5382186cdc
jaafari-R <jaafariramadan@gmail.com>
2024-03-13 02:50:33 +0200

    removed unnecessary validator

--- BackEnd/validators/case.validate.js
+++
@@ -1,8 +0,0 @@
-import GenericValidator from "./generic.validate.js";
-
-export default class CaseValidator {
-    static validateCaseData(data) {
-        Object.keys(data).forEach(key => 
-            GenericValidator.validateNotEmpty(data[key]));
-    }
-}
