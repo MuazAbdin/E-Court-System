d7ccb2f3b20a25b4901cc071622d0a42e92d5a23
jaafari-R <jaafariramadan@gmail.com>
2024-03-17 16:56:41 +0200

    Implemented CaseRespond Controllers

--- BackEnd/controllers/caseResponds.controller.js
+++ BackEnd/controllers/caseResponds.controller.js
@@ -3,7 +3,10 @@ import errorHandler from "../errors/errorHandler.js";
 import Case from "../models/case.model";
 import CaseRespond from "../models/caseRespond.model";
 import GenericValidator from "../validators/generic.validate.js";
-import { CaseDoesNotHaveARespondantPartyError, RespondantPartyAlreadyHasALawyerError } from "../errors/caseRespond.error.js";
+import { CaseDoesNotHaveARespondantPartyError, CaseRespondDoesNotExistError, NoCaseRespondsFoundError, RespondantPartyAlreadyHasALawyerError } from "../errors/caseRespond.error.js";
+import Party from "../models/party.model.js";
+import { PartyDoesNotExistError } from "../errors/party.error.js";
+import { CaseDoesNotExistError } from "../errors/case.error.js";
 
 class CaseRespondController {
     async createCaseRespond(req, res) {
@@ -12,12 +15,14 @@ class CaseRespondController {
         try {
             GenericValidator.validateObjectId(caseId);
 
-            // make sure the case doesn't have a caseRespondant Lawyer
-            const case_ = Case.findById(caseId).populate("parties");
-            if(!parties[1]) {
+            const case_ = await Case.findById(caseId).populate("parties").exec();
+            if(case_ === null) {
+                throw new CaseDoesNotExistError();
+            }
+            if(!case_.parties[1]) {
                 throw new CaseDoesNotHaveARespondantPartyError();
             }
-            if(parties[1].lawyer !== null) {
+            if(case_.parties[1].lawyer !== null) {
                 throw new RespondantPartyAlreadyHasALawyerError();
             }
 
@@ -31,7 +36,11 @@ class CaseRespondController {
 
     async getAllCaseRespond(req, res) {
         try {
-
+            const caseResponds = await Case.find({}).populate("lawyer case");
+            if(caseResponds.length === 0) {
+                throw new NoCaseRespondsFoundError();
+            }
+            res.json(caseResponds);
         }
         catch(error) {
             errorHandler.handleError(res, error);
@@ -39,8 +48,35 @@ class CaseRespondController {
     }
 
     async reviewCaseRespond(req, res) {
+        const { approve, respondId } = req.body;
         try {
+            GenericValidator.validateObjectId(respondId);
 
+            if(approve) {
+                const caseRespond = await CaseRespond.findById(respondId)
+                    .populate({ path: "case", populate: { path: "parties"}}).exec();
+                if(caseRespond === null) {
+                    throw new CaseRespondDoesNotExistError();
+                }
+                if(caseRespond.case === null) {
+                    throw new CaseDoesNotExistError();
+                }
+                if(!caseRespond.case.parties[1]) {
+                    throw new CaseDoesNotHaveARespondantPartyError();
+                }
+                if(caseRespond.case.parties[1].lawyer !== null) {
+                    throw new RespondantPartyAlreadyHasALawyerError();
+                }
+    
+                const party = await Party.findByIdAndUpdate(caseRespond.parties[1]._id, 
+                    { $set: { lawyer: caseRespond.lawyer } });
+                if(party === null) {
+                    throw new PartyDoesNotExistError();
+                }
+            }
+
+            await CaseRespond.findByIdAndDelete(respondId);
+            res.sendStatus(StatusCodes.NO_CONTENT);
         }
         catch(error) {
             errorHandler.handleError(res, error);
