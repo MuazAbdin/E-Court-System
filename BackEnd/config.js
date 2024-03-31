8fddc568a6a6706789894c27f646e265cc240deb
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-03-31 04:25:19 +0300

    update

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -4,7 +4,7 @@ config();
 export default class Config {
     
     static PORT = process.env.PORT || 4000;
-    static DB_URL = process.env.DB_URL || "mongodb://127.0.0.1/justice-overseer";
+    static DB_URL = process.env.DB_URL;
     static HASH_PASSWORD_SALT_ROUNDS = process.env.HASH_PASSWORD_SALT_ROUNDS || 10;
     static JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "1234";
     static JWT_EXPIRE_TIME = process.env.JWT_EXPIRE_TIME || 60 * 60 * 24 * 7;
