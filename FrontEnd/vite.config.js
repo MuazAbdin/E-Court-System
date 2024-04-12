06b79adb0960273203b7c86e166e8777e77c60a8
jaafari-R <jaafariramadan@gmail.com>
2024-04-12 16:52:47 +0300

    SET BASE_URL as an env variable

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       "process.env.GOOGLE_CLIENT_ID": JSON.stringify(env.GOOGLE_CLIENT_ID),
+      "process.env.BASE_URL": JSON.stringify(env.BASE_URL),
     },
   };
 });
